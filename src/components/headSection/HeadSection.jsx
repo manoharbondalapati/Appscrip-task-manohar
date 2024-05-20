8555d34f6745fa1227b3a0d592a2e9209952b7d6
manoharbondalapati <manohar.bondalapati34@gmail.com>
2024-05-20 17:28:16 +0530

    updated files

--- src/components/headSection/HeadSection.jsx
+++ src/components/headSection/HeadSection.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import "./style.css";
 const HeadSection = () => {
   return (
-    <>
+    <div className="headSection-outer">
       <div className="mobile-header">
         <p>
           <span>HOME</span> | SHOP
         </p>
       </div>
-      <div className="headSection-outer">
         <h1>DISCOVER OUR PRODUCTS</h1>
         <p>Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus</p>
         <p id="mobile">
           Dolor integer scelerisque nibh amet mi ut elementum dolor scelerisque.
         </p>
       </div>
-    </>
+    
   );
 };
 
