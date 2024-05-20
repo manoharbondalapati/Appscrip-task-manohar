5819d4952a333db079cb53ad44311d0852a26bcd
manoharbondalapati <manohar.bondalapati34@gmail.com>
2024-05-20 17:46:35 +0530

    updated files

--- src/components/headSection/HeadSection.jsx
+++ src/components/headSection/HeadSection.jsx
@@ -4,9 +4,7 @@ const HeadSection = () => {
   return (
     <div className="headSection-outer">
       <div className="mobile-header">
-        <p>
-          <span>HOME</span> | SHOP
-        </p>
+      <span className='home'><span className='home-opacity'>HOME &nbsp; </span>| &nbsp; SHOP</span>
       </div>
         <h1>DISCOVER OUR PRODUCTS</h1>
         <p>Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus</p>
